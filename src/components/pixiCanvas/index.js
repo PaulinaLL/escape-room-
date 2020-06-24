1e6feb69b54f90ea172a0d981eece7bee73adc34
geraldjän <solidcaius@gmail.com>
2020-06-24 21:33:25 +0200

    set in first object

--- src/components/pixiCanvas/index.js
+++ src/components/pixiCanvas/index.js
@@ -4,25 +4,35 @@ import ROOM from "../../assets/rooms/Corner.jpg";
 
 
 function PixiCanvas() {
+
+  let app = new PIXI.Application({ width: 512, height: 384 });
   
+  const background = PIXI.Sprite.from(ROOM);
+  background.width = 512;
+  background.height = 384;
+
+  const sprite = PIXI.Sprite.from(ROOM);
+  sprite.width = 50;
+  sprite.height = 50;
+
+  sprite.x = 80;
+  sprite.y = 110;
+
+
+
   useLayoutEffect(() => {
     
-    let app = new PIXI.Application({ width: 512, height: 384 });
-
     const container = document.querySelector("#pixi-container");
     //Create a Pixi Application
     //Add the canvas that Pixi automatically created for you to the HTML document
-    container.appendChild(app.view);
-    
-    const background = PIXI.Sprite.from(ROOM);
-    background.width = 512;
-    background.height = 384;
-    app.stage.addChild(background);
+    container.appendChild(app.view);    
+    app.stage.addChild(background,sprite);
 
   });
 
-
-
+function light() {
+ background.width = 10; 
+}
   return <div id="pixi-container"></div>;
 }
 
