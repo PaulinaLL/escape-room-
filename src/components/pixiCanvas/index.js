75b0062af22017ad0b1b5983909b335e2c9f7162
geraldjän <solidcaius@gmail.com>
2020-06-25 14:54:23 +0200

    made parts of spritesheet available

--- src/components/pixiCanvas/index.js
+++ src/components/pixiCanvas/index.js
@@ -1,6 +1,7 @@
 import React, { useLayoutEffect } from "react";
 import * as PIXI from "pixi.js";
 import ROOM from "../../assets/rooms/Corner.jpg";
+import SPRITES from "../../assets/objects/Spritesheet.png";
 
 
 function PixiCanvas() {
@@ -10,31 +11,59 @@ function PixiCanvas() {
   const background = PIXI.Sprite.from(ROOM);
   background.width = 512;
   background.height = 384;
-
-  const sprite = PIXI.Sprite.from(ROOM);
-  sprite.width = 50;
-  sprite.height = 50;
-
-  sprite.x = 80;
-  sprite.y = 110;
+  let furnitureSheet = {};
 
 
+
+function doneLoading(e) {
+createFurnitureSheet();
+createDrawer();
+}
+
+function createFurnitureSheet(){
+  let fsheet = new PIXI.BaseTexture.from(app.loader.resources["furniture"].url);
+  let w = 21;
+  let h = 50;
+
+furnitureSheet["closed"]=
+[ new PIXI.Texture(fsheet, new PIXI.Rectangle(0 * w, 0, w, h))];
+
+furnitureSheet["open1"]=
+[ new PIXI.Texture(fsheet, new PIXI.Rectangle(1 * w, 0, w, h))];
+
+furnitureSheet["open2"]=
+[ new PIXI.Texture(fsheet, new PIXI.Rectangle(2 * w, 0, w, h))];
+
+furnitureSheet["open3"]=
+[ new PIXI.Texture(fsheet, new PIXI.Rectangle(3 * w, 0, w, h))];
+
+furnitureSheet["open4"]=
+[  new PIXI.Texture(fsheet, new PIXI.Rectangle(4 * w, 0, w, h))];
+}
 
   useLayoutEffect(() => {
+
+    app.loader.add("furniture", SPRITES)
+    app.loader.load(doneLoading);
     
+  });
+
+  function createDrawer() {
+    const drawer = new PIXI.AnimatedSprite(furnitureSheet.closed);
+    drawer.width = 200;
+    drawer.height = 200;
+   
+// Might be turned back to normal. Lets see.
     const container = document.querySelector("#pixi-container");
     //Create a Pixi Application
     //Add the canvas that Pixi automatically created for you to the HTML document
     container.appendChild(app.view);    
-    app.stage.addChild(background,sprite);
+    app.stage.addChild(background,drawer); 
+ 
 
-  });
+    };
 
   return <div id="pixi-container"></div>;
 }
-
-
-
-
 
 export default PixiCanvas;
