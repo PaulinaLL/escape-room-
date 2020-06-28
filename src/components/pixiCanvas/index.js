bf823c71ff81c46a10d14fde6565f8bbff9ff939
gerald <solidcaius@gmail.com>
2020-06-28 23:15:14 +0200

    added png

--- src/components/pixiCanvas/index.js
+++ src/components/pixiCanvas/index.js
@@ -1,8 +1,8 @@
 import React, { useLayoutEffect } from "react";
 import * as PIXI from "pixi.js";
 import ROOM from "../../assets/rooms/Corner.jpg";
-import SPRITES from "../../assets/objects/Spritesheet.png";
-
+import SPRITES from "../../assets/objects/Drawer2.png";
+ 
 
 function PixiCanvas() {
 
@@ -20,8 +20,8 @@ createDrawer();
 
 function createFurnitureSheet(){
   let fsheet = new PIXI.BaseTexture.from(app.loader.resources["furniture"].url);
-  let w = 21;
-  let h = 50;
+  let w = 320;
+  let h = 400; 
 
 furnitureSheet["closed"]=
 [ new PIXI.Texture(fsheet, new PIXI.Rectangle(0 * w, 0, w, h))];
@@ -43,13 +43,15 @@ furnitureSheet["open4"]=
 
     app.loader.add("furniture", SPRITES)
     app.loader.load(doneLoading);
-    
+  
   });
 
   function createDrawer() {
     const drawer = new PIXI.AnimatedSprite(furnitureSheet.open1);
-    drawer.width = 200;
-    drawer.height = 200;
+    drawer.width = 145;
+    drawer.height = 160;
+    drawer.x = 370;
+    drawer.y = 185;
    
 // Might be turned back to normal. Lets see.
     const container = document.querySelector("#pixi-container");
@@ -57,7 +59,6 @@ furnitureSheet["open4"]=
     //Add the canvas that Pixi automatically created for you to the HTML document
     container.appendChild(app.view);    
     app.stage.addChild(background,drawer); 
-
     };
 
   return <div id="pixi-container"></div>;
