1278376aebe122505e6d2510319ba97848f7a0bf
PaulinaLL <paulinalll90@wp.pl>
2020-07-02 16:04:20 +0200

    add createPC function

--- src/helper/createObjects.js
+++ src/helper/createObjects.js
@@ -1,32 +1,28 @@
 import * as PIXI from "pixi.js";
-import {leftOver} from "./buttons.js";
+import { leftOver } from "./buttons.js";
+import { displayAlert } from "./pc.js";
 
+export function createUserInterface(arrowSheet, app) {
+  const left = new PIXI.Sprite(arrowSheet.left[0]);
+  const right = new PIXI.Sprite(arrowSheet.right[0]);
 
-export function createUserInterface(arrowSheet, app){
+  left.interactive = true;
+  left.buttonMode = true;
 
-const left = new PIXI.Sprite(arrowSheet.left[0]);
-const right = new PIXI.Sprite(arrowSheet.right[0]);
+  left.on("pointerover", leftOver);
 
-left.interactive = true;
-left.buttonMode = true;
+  right.interactive = true;
+  right.buttonMode = true;
 
-left
-    .on("pointerover", leftOver);
+  right.scale.set(-1);
 
-right.interactive = true;
-right.buttonMode = true;
-    
+  right.x = 550;
+  right.y = 625;
+  left.x = 200;
+  left.y = 500;
 
-right.scale.set(-1);
-
-right.x = 550;
-right.y = 625;
-left.x = 200;
-left.y = 500;
-
-return [left,right];
+  return [left, right];
 }
-
 
 export function createDrawer(furnitureSheet) {
   const drawer = new PIXI.AnimatedSprite(furnitureSheet.closed);
@@ -55,18 +51,31 @@ export function createDrawer(furnitureSheet) {
       return;
     }
     this.texture = furnitureSheet.closed[0];
+  }
+  //    It would be good if we get the relationship between the values. Please do something about it.
+  // Width and Height
+  drawer.width = 200;
+  drawer.height = 260;
+  //    Position
+  drawer.x = 530;
+  drawer.y = 295;
+  // Might be turned back to normal. Lets see.
+  //Create a Pixi Application
+  //Add the canvas that Pixi automatically created for you to the HTML document
+  return drawer;
 }
-//    It would be good if we get the relationship between the values. Please do something about it.   
-// Width and Height
-    drawer.width = 200;
-    drawer.height = 260;
-//    Position
-    drawer.x = 530;
-    drawer.y = 295;
-// Might be turned back to normal. Lets see.
-    //Create a Pixi Application
-    //Add the canvas that Pixi automatically created for you to the HTML document
-        return drawer;
-};
 
+// add event listener to PC version 1
+export function createPC(pcSheet) {
+  const pcoff = new PIXI.Sprite(pcSheet.off[0]);
 
+  pcoff.interactive = true;
+  pcoff.buttonMode = true;
+
+  pcoff.on("pointerdown", displayAlert);
+
+  pcoff.x = 50;
+  pcoff.y = 110;
+
+  return pcoff;
+}
