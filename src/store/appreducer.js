46f1837b355d958720453590081e4419a4d3987c
PaulinaLL <paulinalll90@wp.pl>
2020-07-03 15:54:01 +0200

    add 2 new actions to reducer

--- src/store/appreducer.js
+++ src/store/appreducer.js
@@ -1,25 +1,29 @@
 import data from "./exitGame.json";
-import produce from "immer"
+import produce from "immer";
 
-const appReducer = produce((draft = data, action) =>{
-  switch (action.type) { 
-          case "SELECT":
-          return draft;
-          case "SWITCHLEFT":
+const appReducer = produce((draft = data, action) => {
+  switch (action.type) {
+    case "SELECT":
+      return draft;
+    case "SWITCHLEFT":
+      draft.partNumber =
+        draft.numberOfParts === draft.partNumber + 1 ? 0 : draft.partNumber + 1;
+      return draft;
+    case "SWITCHRIGHT":
+      console.log(draft.numberOfParts);
+      draft.partNumber = draft.partNumber - 1 === -1 ? 1 : draft.partNumber - 1;
+      return draft;
+    case "SELECTPC":
+      draft.riddleDescription = draft.riddleDescription.first;
+      return draft;
+    case "SELECTDRAWER":
+      // console.log(draft.riddleDescription);
+      draft.riddleDescription = draft.riddleDescription.second;
+      return draft;
 
-          draft.partNumber = draft.numberOfParts === draft.partNumber + 1? 0: draft.partNumber + 1;
-          
-          return draft;
-          case "SWITCHRIGHT":
-            console.log(draft.numberOfParts);
-          draft.partNumber = draft.partNumber - 1 === -1 ? 1: draft.partNumber - 1;
-
-          return draft;
-          default: 
-          return draft;
-  } 
-})
-
-
+    default:
+      return draft;
+  }
+});
 
 export default appReducer;
