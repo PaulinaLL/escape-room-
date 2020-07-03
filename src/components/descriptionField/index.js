709e36ddd00e26bb9c66af956d785e59a569c641
PaulinaLL <paulinalll90@wp.pl>
2020-07-03 15:57:01 +0200

    trying to get the state into description component

--- src/components/descriptionField/index.js
+++ src/components/descriptionField/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./index.scss";
+import store from "../../store/store.js";
+import { connect } from "react-redux";
 
-export default function DescriptionField() {
+ const DescriptionField = () => {
+  console.log("store", store);
+  const state = store.getState();
+  console.log("state", state);
+
   return (
     <div className="description">
       <p>description will appear here</p>
     </div>
   );
 }
+
+export connect()(DescriptionField)
