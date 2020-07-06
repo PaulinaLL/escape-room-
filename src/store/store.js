29b325b0aa58c420718c0d5689c88308c32c7e8f
PaulinaLL <paulinalll90@wp.pl>
2020-07-06 20:03:49 +0200

    Add newReducer to the store

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
-import assetReducer from './appreducer';
+import { configureStore } from "@reduxjs/toolkit";
+import assetReducer from "./appreducer";
+import newReducer from "./newReducer";
 
-export default configureStore({
-  reducer: {
-    assetReducer
+export default configureStore(
+  {
+    reducer: {
+      assetReducer,
+      newReducer,
+    },
   },
-},
-window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
