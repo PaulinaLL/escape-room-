1c5471c57e4c2a0c4253571a37662f75b41ca7c1
PaulinaLL <paulinalll90@wp.pl>
2020-07-06 09:37:20 +0200

    trying mapStateToProps

--- src/components/descriptionField/index.js
+++ src/components/descriptionField/index.js
@@ -3,16 +3,23 @@ import "./index.scss";
 import store from "../../store/store.js";
 import { connect } from "react-redux";
 
- const DescriptionField = () => {
-  console.log("store", store);
-  const state = store.getState();
-  console.log("state", state);
-
+const DescriptionField = () => {
+  // console.log("store", store);
+  // const state = store.getState();
+  // console.log("state", state);
+  console.log("store.getState:", store.getState());
   return (
     <div className="description">
       <p>description will appear here</p>
     </div>
   );
+};
+
+function mapStateToProps(state) {
+  // const state = store.getState();
+  // console.log("state", state);
+  const { description } = state;
+  return { assetReducer };
 }
 
-export connect()(DescriptionField)
+export default connect(mapStateToProps)(DescriptionField);
