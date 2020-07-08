d4306029c544b37e43d0da6c3b304d36bc097f73
PaulinaLL <paulinalll90@wp.pl>
2020-07-08 14:27:55 +0200

    magic

--- src/components/userInput/index.js
+++ src/components/userInput/index.js
@@ -2,10 +2,32 @@ import React, { useState } from "react";
 import "./index.scss";
 import { useDispatch, useSelector } from "react-redux";
 
+const preInputSetup = (userName) => (id) => {
+  const inputs = {
+    1: `function greet(person) {
+    if (person == { name: ${userName} }) {
+    return "hey " +  ${userName};
+    } else {
+    return "hey stranger";
+    }
+    }
+    console.log(greet({ name: ${userName} }))`,
+    2: `test for the second function`,
+  };
+
+  return inputs[id] || null;
+};
+
 export default function UserInput() {
+  const { userName, currentRiddleDescription } = useSelector(
+    (state) => state.answersReducer
+  );
+
+  const preInputs = preInputSetup(userName);
+
   const [userAnswer, setUserAnswer] = useState();
+
   const dispatch = useDispatch();
-  const { answersReducer } = useSelector((state) => state);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -18,19 +40,7 @@ export default function UserInput() {
   };
 
   // const storeState = store.getState();
-  console.log(
-    "here from userInput ",
-    answersReducer.currentRiddleDescription.id
-  );
-
-  const preInput = `function greet(person) {
-    if (person == { name: ${answersReducer.userName} }) {
-    return "hey " +  ${answersReducer.userName};
-    } else {
-    return "hey stranger";
-    }
-    }
-    console.log(greet({ name: ${answersReducer.userName} }))`;
+  console.log("here from userInput", currentRiddleDescription.id);
 
   return (
     <div className="userInput">
@@ -42,9 +52,8 @@ export default function UserInput() {
           name="userInput"
           rows="15"
           cols="70"
-          defaultValue={
-            answersReducer?.currentRiddleDescription.id === 1 ? preInput : ""
-          }
+          defaultValue={preInputs(currentRiddleDescription.id)}
+          value={userAnswer}
           onChange={(e) => setUserAnswer(e.target.value)}
         />
         <input type="submit" value="Submit" onSubmit={handleSubmit}></input>
