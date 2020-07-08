5cd90d24a9697634e31c710cc36f917c1b21808a
PaulinaLL <paulinalll90@wp.pl>
2020-07-08 11:44:55 +0200

    displays description first after click on start button

--- src/components/descriptionField/index.js
+++ src/components/descriptionField/index.js
@@ -1,15 +1,20 @@
 import React from "react";
 import "./index.scss";
 import { useSelector } from "react-redux";
+import store from "../../store/store";
 
 const DescriptionField = () => {
   const { answersReducer } = useSelector((state) => state);
 
-  // console.log("store.getState:", store.getState());
+  const storeState = store.getState();
 
   return (
     <div className="description">
-      <p>{answersReducer.currentRiddleDescription.text}</p>
+      <p>
+        {storeState.answersReducer.wantsToPlay
+          ? answersReducer.currentRiddleDescription.text
+          : null}
+      </p>
     </div>
   );
 };
