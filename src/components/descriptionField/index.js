0a5050200d30aa4b072679bbbfab617ce3e9d512
PaulinaLL <paulinalll90@wp.pl>
2020-07-08 10:18:48 +0200

    remove console.logs, change reducer

--- src/components/descriptionField/index.js
+++ src/components/descriptionField/index.js
@@ -1,26 +1,17 @@
 import React from "react";
 import "./index.scss";
-// import store from "../../store/store.js";
-// import { connect } from "react-redux";
 import { useSelector } from "react-redux";
 
 const DescriptionField = () => {
-  const { assetReducer } = useSelector((state) => state);
+  const { answersReducer } = useSelector((state) => state);
 
   // console.log("store.getState:", store.getState());
 
   return (
     <div className="description">
-      <p>{assetReducer.currentRiddle.text}</p>
+      <p>{answersReducer.currentRiddleDescription.text}</p>
     </div>
   );
 };
-
-// function mapStateToProps(state) {
-//   const { description } = state;
-//   console.log(store.getState())
-//   return {};
-// }
-// export default connect(mapStateToProps)(DescriptionField)
 
 export default DescriptionField;
