800b65efdb3442a1f803314200dd98eef306b4a2
PaulinaLL <paulinalll90@wp.pl>
2020-07-08 10:22:16 +0200

    spread state before changing the state + add new cases

--- src/store/answersReducer.js
+++ src/store/answersReducer.js
@@ -1,12 +1,21 @@
+import data from "./exitGame.json";
+
 const initialState = {
   userName: null,
   userAnswers: [],
+  currentRiddleDescription: {
+    id: null,
+    text:
+      "it seems like the computer is on... check if there is some info to find",
+    img: null,
+  },
 };
 
 function answersReducer(state = initialState, action) {
   switch (action.type) {
     case "ADD_USER_NAME":
       return {
+        ...state,
         userName: action.payload.name,
       };
     case "ADD_USER_ANSWER":
@@ -14,11 +23,28 @@ function answersReducer(state = initialState, action) {
         ...state,
         userAnswers: [...state.userAnswers, action.payload.answer],
       };
+    case "SELECT_PC":
+      return {
+        ...state,
+        currentRiddleDescription: {
+          id: 1,
+          text: data.riddleDescription.first.text,
+          img: data.riddleDescription.first.img,
+        },
+      };
+    case "SELECT_DRAWER":
+      return {
+        ...state,
+        currentRiddleDescription: {
+          id: 2,
+          text: data.riddleDescription.second.text,
+          img: null,
+        },
+      };
+
     default:
       return state;
   }
 }
-//   case "GETS_CURRENT_RIDDLE_DESCRIPTION":
-//     return draft;
 
 export default answersReducer;
