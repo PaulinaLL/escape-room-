01536a0a0bc18ee2546b9788d2903e35ecf28c8e
PaulinaLL <paulinalll90@wp.pl>
2020-07-09 17:25:39 +0200

    console.log uneccessary

--- src/helper/pc.js
+++ src/helper/pc.js
@@ -4,23 +4,18 @@
 //   alert("Hello stranger!");
 // }
 
-import store from "../store/store";
+// import store from "../store/store";
 
 export function pcRiddleDisplay() {
-  const storeState = store.getState();
-
-  function greet(person) {
-    // if (person.name === storeState.answersReducer.userName)
-    if (person === { name: storeState.answersReducer.userName }) {
-      return "hey" + storeState.answersReducer.userName;
-    } else {
-      return "hey stranger";
-    }
-  }
-
-  console.log(greet({ name: storeState.answersReducer.userName }));
-
+  // const storeState = store.getState();
+  // function greet(person) {
+  //   if (person === { name: storeState.answersReducer.userName }) {
+  //     return "hey" + storeState.answersReducer.userName;
+  //   } else {
+  //     return "hey stranger";
+  //   }
+  // }
+  // console.log(greet({ name: storeState.answersReducer.userName }));
   // console.log(storeState.answersReducer.userName);
-
   // console.log("pc riddle", storeState);
 }
