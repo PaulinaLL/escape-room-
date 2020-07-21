47be67c50fb8a80d0bcb476c02e700ee838fcaa8
PaulinaLL <paulinalll90@wp.pl>
2020-07-21 14:13:54 +0200

    commented out- probabaly we dont need this file

--- src/components/evalInput/index.js
+++ src/components/evalInput/index.js
@@ -1,26 +1,26 @@
-import React from "react";
-import store from "../../store/store";
-import { useSelector } from "react-redux";
-import data from "../../store/exitGame.json";
+// import React from "react";
+// import store from "../../store/store";
+// import { useSelector } from "react-redux";
+// import data from "../../store/exitGame.json";
 
-export default function EvalInput() {
-  console.log(store);
-  const { userAnswers, currentRiddleDescription } = useSelector(
-    (state) => state.answersReducer
-  );
-  // console.log(
-  //   Object.values(userAnswers[currentRiddleDescription.id]).toString()
-  // );
+// export default function EvalInput() {
+//   console.log(store);
+//   const { userAnswers, currentRiddleDescription } = useSelector(
+//     (state) => state.answersReducer
+//   );
+//   // console.log(
+//   //   Object.values(userAnswers[currentRiddleDescription.id]).toString()
+//   // );
 
-  // const evaluation = new Function()
+//   // const evaluation = new Function()
 
-  return Object.values(userAnswers[currentRiddleDescription.id]).toString() ===
-    data.riddleDescription[currentRiddleDescription.id].correctAnswer ? (
-    <p>success</p>
-  ) : (
-    <p>failure</p>
-  );
-}
+//   return Object.values(userAnswers[currentRiddleDescription.id]).toString() ===
+//     data.riddleDescription[currentRiddleDescription.id].correctAnswer ? (
+//     <p>success</p>
+//   ) : (
+//     <p>failure</p>
+//   );
+// }
 
 // new Function('a', 'b', 'return a + b'); // basic syntax
 // new Function('a,b', 'return a + b'); // comma-separated
