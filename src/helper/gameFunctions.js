06e3ad752271f93e54b65ec1bcb0b9423fc79b1f
PaulinaLL <paulinalll90@wp.pl>
2020-07-22 19:23:33 +0200

    commented out

--- src/helper/gameFunctions.js
+++ src/helper/gameFunctions.js
@@ -1,6 +1,6 @@
-export function takeIDCard2() {
-    //5 is idCArd 2 - yellow card (without flashlight 4)
-   let yellowCard = roomBack.children[4];
-    yellowCard.visible = false;
-    dispatch({ type: "TAKE_IDCARD2" });
-  }
+// export function takeIDCard2() {
+//     //5 is idCArd 2 - yellow card (without flashlight 4)
+//    let yellowCard = roomBack.children[4];
+//     yellowCard.visible = false;
+//     dispatch({ type: "TAKE_IDCARD2" });
+//   }
