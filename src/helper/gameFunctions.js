ba53d6cce060c2a48a66abee006c0bc2173b3706
PaulinaLL <paulinalll90@wp.pl>
2020-07-31 22:37:39 +0200

    remove unecessery file

--- src/helper/gameFunctions.js
+++
@@ -1,6 +0,0 @@
-// export function takeIDCard2() {
-//     //5 is idCArd 2 - yellow card (without flashlight 4)
-//    let yellowCard = roomBack.children[4];
-//     yellowCard.visible = false;
-//     dispatch({ type: "TAKE_IDCARD2" });
-//   }
